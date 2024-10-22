52440587958f42b294e37f943d3f16c94647e29f
RencyDuqs <dayneduque22@gmail.com>
2024-10-22 11:37:37 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+function Key({ label, clickHandler }) {
+  return (
+    <button onClick={clickHandler}>
+      {label}
+    </button>
+  );
+}
+
+function Display({ display }) {
+  return (
+    <div className="DispContainer">
+      {display}
+    </div>
+  );
+}
+
+function NameDisplay({ name }) {
+  return (
+    <div className="nameDisplay">
+      {name}
+    </div>
+  );
+}
 
 function App() {
+  const [disp, setDisp] = useState("0 Things That Require Zero Talent");
+  const [name] = useState("Rency Dayne M. Duque");
+
+  const items = [
+    "Being On Time",
+    "Making An Effort",
+    "Being High Energy",
+    "Having A Positive Attitude",
+    "Being Passionate",
+    "Using Good Body Language",
+    "Being Coachable",
+    "Doing A Little Extra",
+    "Being Prepared",
+    "Having A Strong Work Ethic"
+  ];
+
+  const itemClickHandler = (index) => {
+    setDisp(items[index]);
+  };
+
+  const resetClickHandler = () => {
+    setDisp("10 Things That Require Zero Talent");
+  };
+
+  const nameClickHandler = () => {
+    setDisp(name.toUpperCase());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="CalcContainer">
+        <NameDisplay name={`${name} - BSIT 3-A`} />
+        <Display display={disp} />
+        <div className="ButtonsContainer">
+          {items.map((_, index) => (
+            <Key key={index} label={index + 1} clickHandler={() => itemClickHandler(index)} />
+          ))}
+          <Key label={"0"} clickHandler={() => setDisp("0")} />
+          <Key label={"RESET"} clickHandler={resetClickHandler} />
+          <Key label={"NAME"} clickHandler={nameClickHandler} />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
